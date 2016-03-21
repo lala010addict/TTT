b8df4978f4583434094f0549b25eb66d5ff08344
jieyingpeng <jipeng@syr.edu>
2016-03-21 13:13:09 -0700

    server

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ var server = http.createServer(app);
 var io = require('socket.io').listen(server);  //pass a http.Server instance
 
 
-app.get('/', function (req, res) {
-  res.sendFile('client/index.html');
+app.get('/', function(req, res) {
+    res.sendfile('client/index.html');
 });
+
+ io.sockets.on('connection', function (socket) {
+    
+
+    // Fire the init message to setup the game
+    socket.emit('data', {event:'init', ok:true, result: socket.handshake.sessionID});
+  });
+
+
 
 app.listen(8080, function () {
   console.log('App listening on port 8080!');
